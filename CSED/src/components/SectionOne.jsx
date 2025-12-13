bbd7fac1b79ff9b767b24030014eb25af812bd2b
ashvant-sunish <ashvantsunish.development@gmail.com>
2025-12-13 14:01:57 +0530

    Initial Commit

---
+++ CSED/src/components/SectionOne.jsx
@@ -0,0 +1,13 @@
+// For Welcome Screen and About Section
+
+import React from 'react'
+
+function SectionOne() {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default SectionOne
---
+++ CSED/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionOne from '../components/SectionOne'
+
+function Home() {
+  return (
+    <div>
+      <SectionOne />
+    </div>
+  )
+}
+
+export default Home
