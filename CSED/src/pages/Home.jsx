735139d5e4f213d580da2b56031040f58b36be28
ashvant-sunish <ashvantsunish.development@gmail.com>
2025-12-19 01:39:30 +0530

    Section 1.0 update

--- CSED/src/pages/Home.jsx
+++ CSED/src/pages/Home.jsx
@@ -1,13 +1,7 @@
-import SectionFour from "../components/SectionFour/sectionFour";
-import SectionOne from "../components/SectionOne/SectionOne";
-import SectionTwo from "../components/SectionTwo/SectionTwo";
 
 function Home() {
   return (
     <div>
-      <SectionOne />
-      <SectionTwo />
-      <SectionFour />
     </div>
   )
 }
